986d790546cc087e9158b7190770745052618cb1
OokamiMidori <fabio.miranda.couto@gmail.com>
2022-09-06 18:06:43 -0300

    exercicios

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,24 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Escreva aqui a sua nacionalidade").toLowerCase();
+
+
+if (nacionalidade == "brasileira") {
+  console.log("Nacionalidade brasileira");
+} else {
+  if (nacionalidade == "argentina") {
+    console.log("Nacionalidade argentina");
+  } else {
+    if (nacionalidade == "uruguaia") {
+      console.log("Nacionalidade Uruguaia");
+    } else {
+      if (nacionalidade == "chilena") {
+        console.log("Nacionalidade chilena");
+      } else {
+        if (nacionalidade == "colombiana") {
+          console.log("Nacionalidade colombiana");
+        } else {
+          console.log("Nacionalidade não encontrada");
+        }
+      }
+    }
+  }
+}
